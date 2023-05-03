85d29a0b90563da7a153aa13d7b510f457a193e9
Kaitlin Lovrich <kaylovrich@gmail.com>
2023-05-03 16:34:59 -0700

    Set up views and urls to work with html templates

--- django_delights/inventory/views.py
+++ django_delights/inventory/views.py
@@ -1,3 +1,89 @@
 from django.shortcuts import render
+from .models import MenuItem, Ingredient, RecipeRequirement, Purchase
+from .forms import MenuItemForm, IngredientForm, RecipeRequirementForm, PurchaseForm
+from django.views.generic import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'inventory/home.html')
+
+# MenuItem views
+class MenuItemList(ListView):
+    model = MenuItem
+    template_name = 'inventory/menu.html'
+  
+class MenuItemCreate(CreateView):
+    model = MenuItem
+    template_name = 'inventory/add-menu-item.html'
+    form_class = MenuItemForm
+
+class MenuItemUpdate(UpdateView):
+    model = MenuItem
+    template_name = 'inventory/edit-menu-item.html'
+    form_class = MenuItemForm
+    success_url = 'inventory/menu.html'
+    
+class MenuItemDelete(DeleteView):
+    model = MenuItem
+    template_name = 'inventory/delete-menu-item.html'
+    success_url = 'inventory/menu.html'
+
+# Ingredient views
+class IngredientList(ListView):
+    model = Ingredient
+    template_name = 'inventory/ingredients.html'
+
+class IngredientCreate(CreateView):
+    model = Ingredient
+    template_name = 'inventory/add-ingredient.html'
+    form_class = IngredientForm
+
+class IngredientUpdate(UpdateView):
+    model = Ingredient
+    template_name = 'inventory/edit-ingredient.html'
+    form_class = IngredientForm
+    success_url = 'inventory/ingredients.html'
+    
+class IngredientDelete(DeleteView):
+    model = Ingredient
+    template_name = 'inventory/delete-ingredient.html'
+    success_url = 'inventory/ingredients.html'
+
+# RecipeRequirement views
+class RecipeRequirementCreate(CreateView):
+    model = RecipeRequirement
+    template_name = 'inventory/add-recipe-requirement.html'
+    form_class = RecipeRequirementForm
+    
+class RecipeRequirementUpdate(UpdateView):
+    model = RecipeRequirement
+    template_name = 'inventory/edit-recipe-requirement.html'
+    form_class = RecipeRequirementForm
+    success_url = 'inventory/menu.html'
+    
+class RecipeRequirementDelete(DeleteView):
+    model = RecipeRequirement
+    template_name = 'inventory/delete-recipe-requirement.html'
+    success_url = 'inventory/menu.html'
+
+# Purchase views
+class PurchaseList(ListView):
+    model = Purchase
+    template_name = 'inventory/purchases.html'
+    
+class PurchaseCreate(CreateView):
+    model = Purchase
+    template_name = 'inventory/add-purchase.html'
+    form_class = PurchaseForm
+    
+class PurchaseUpdate(UpdateView):
+    model = Purchase
+    template_name = 'inventory/edit-purchase.html'
+    form_class = PurchaseForm
+    success_url = 'inventory/purchases.html'
+    
+class PurchaseDelete(DeleteView):
+    model = Purchase
+    template_name = 'inventory/delete-purchase.html'
+    success_url = 'inventory/purchases.html'
